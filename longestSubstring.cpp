9fc3c0c37f1b8ceaa7c1c71b5640ca143db5c237
nikita sharma <150901367+nikitaxarma@users.noreply.github.com>
2025-02-01 15:56:07 +0530

    Create longestSubstring.cpp

---
+++ longestSubstring.cpp
@@ -0,0 +1,23 @@
+#include <unordered_set>
+#include <algorithm>
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        unordered_set<char> charSet;
+        int left = 0, right = 0, maxLength = 0;
+
+        while (right < s.size()) {
+            if (charSet.find(s[right]) == charSet.end()) {
+                charSet.insert(s[right]);
+                maxLength = max(maxLength, right - left + 1);
+                right++;
+            } else {
+                charSet.erase(s[left]);
+                left++;
+            }
+        }
+
+        return maxLength;
+    }
+};
